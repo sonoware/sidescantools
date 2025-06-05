1b543a5d492bbc73530a9b02f2b6592f393e8316
mia.schumacher <mschumacher@geomar.de>
2025-06-05 16:53:15 +0200

    fixed transposing when files wider than long

--- sidescan_georeferencer.py
+++ sidescan_georeferencer.py
@@ -335,13 +335,11 @@ class SidescanGeoreferencer:
 
 
         # Transpose so that the largest axis is horizontal
-        ch_stack = ch_stack if ch_stack.shape[0] < ch_stack.shape[1] else ch_stack.T
-        if False:
-            if swath_len >= swath_width:
-                ch_stack = ch_stack
-                    #ch_stack = ch_stack if ch_stack.shape[0] < ch_stack.shape[1] else ch_stack.T
-            elif swath_len <= swath_width:
-                ch_stack = ch_stack.T
+        #ch_stack = ch_stack if ch_stack.shape[0] < ch_stack.shape[1] else ch_stack.T
+        if swath_len >= swath_width:
+            ch_stack = ch_stack
+        elif swath_len <= swath_width:
+            ch_stack = ch_stack.T
 
         ch_stack = np.array(ch_stack, dtype=float)
 

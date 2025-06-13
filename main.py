5dcac1879a3ebf98e44549843f85a3c624c3be5e
mia.schumacher <mschumacher@geomar.de>
2025-06-13 08:48:39 +0200

    added logo and status bar icon

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from qtpy.QtWidgets import (
     QButtonGroup,
     QComboBox,
 )
-from qtpy.QtGui import QPalette, QColor, QShortcut, QKeySequence
+from qtpy.QtGui import QPalette, QColor, QShortcut, QKeySequence, QPixmap
 import qtpy.QtCore as QtCore
 import qtpy.QtGui as QtGui
 import sys, os, pathlib
@@ -171,12 +171,12 @@ class SidescanToolsMain(QWidget):
         self.egn_table_picker.dir_changed.connect(self.load_egn_table)
         # Caption
         # TODO: Remove preliminary logo until we got a final one
-        # self.title_img = QPixmap("./res/sst_256_cut.png")
-        # self.title_img = self.title_img.scaledToWidth(
-        #     350, QtCore.Qt.TransformationMode.SmoothTransformation
-        # )
-        # self.title_label = QLabel()
-        # self.title_label.setPixmap(self.title_img)
+        self.title_img = QPixmap("./res/sidescantools_logo_eck.png")
+        self.title_img = self.title_img.scaledToWidth(
+            350, QtCore.Qt.TransformationMode.SmoothTransformation
+        )
+        self.title_label = QLabel()
+        self.title_label.setPixmap(self.title_img)
         # File Info
         self.file_info_text_box = QTextEdit()
         self.dir_and_path_label = QLabel("Working directory and paths setup")
@@ -198,8 +198,8 @@ class SidescanToolsMain(QWidget):
         self.left_view.addWidget(self.file_table)
         # right side widgets: meta info, project settings and all parameter
         # TODO: Remove preliminary logo until we got a final one
-        # self.right_view.addWidget(self.title_label)
-        # self.right_view.addWidget(QHLine())
+        self.right_view.addWidget(self.title_label)
+        self.right_view.addWidget(QHLine())
         self.right_view.addWidget(self.file_info_text_box)
         self.right_view.addWidget(QHLine())
         self.right_view.addWidget(self.dir_and_path_label)

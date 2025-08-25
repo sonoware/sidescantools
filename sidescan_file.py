08d5f768995b140cef5f4c95e40104363e0a12a3
finn <finn.spitz@sonoware.de>
2025-08-25 17:45:26 +0200

    Add missing XTF information and ref the docs

--- sidescan_file.py
+++ sidescan_file.py
@@ -8,8 +8,10 @@ from datetime import datetime
 
 
 class SidescanFile:
-    """Wrapper to read sidescan files
+    """Wrapper to read sidescan files.
+    Format definition: https://www.exail.com/media-file/9742/xtf-file-format-specification-rev42.pdf
     """
+
     filepath: Path
     """Path to read file"""
     choose_subsys: int
@@ -52,21 +54,19 @@ class SidescanFile:
     depth: np.ndarray
     """Depth in Meters (Below Water Surface)"""
     packet_no: np.ndarray
-    """Packet Number
-    """
-    # TODO: validate in XTF doc
+    """Packet Number"""
     sensor_heading: np.ndarray
-    """ in degree (?)"""
+    """Sensor heading in degrees"""
     sensor_pitch: np.ndarray
-    """ in degree (?)"""
+    """Pitch in degrees (positive=nose up)"""
     sensor_primary_altitude: np.ndarray
-    """ in m (?)"""
+    """Distance from towfish to the sea floor"""
     sensor_roll: np.ndarray
-    """ in degree (?)"""
+    """Roll in degrees (positive=roll to starboard)"""
     sensor_speed: np.ndarray
-    """ in m/s (?)"""
+    """Speed of towfish in knots"""
     sensor_aux_altitude: np.ndarray
-    """ in degree (?)"""
+    """Auxiliary altitude"""
 
     seconds_per_ping: np.ndarray
     """in s per channel"""
@@ -105,37 +105,82 @@ class SidescanFile:
             # not included
             self.starting_depth = np.zeros(self.num_ping)
             self.gain_adc = np.zeros(self.num_ping)
-            
+
             # read XTF info
             for p_idx in range(self.num_ping):
-                self.sound_velocity[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SoundVelocity * 2 # TODO: this is half SOS in water, right?
+                self.sound_velocity[p_idx] = (
+                    xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SoundVelocity * 2
+                )  # TODO: this is half SOS in water, right?
                 self.timestamp[p_idx] = datetime(
                     xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Year,
                     xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Month,
                     xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Day,
                     xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Hour,
                     xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Minute,
-                    xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Second)
-                self.sensor_heading[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorHeading
-                self.sensor_pitch[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorPitch
-                self.sensor_primary_altitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorPrimaryAltitude
-                self.sensor_roll[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorRoll
-                self.sensor_speed[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorSpeed
-                self.sensor_aux_altitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorAuxAltitude
-                self.longitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorXcoordinate
-                self.latitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorYcoordinate
-                self.depth[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].SensorDepth
-                self.packet_no[p_idx]  = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].PingNumber
+                    xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].Second,
+                )
+                self.sensor_heading[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorHeading
+                self.sensor_pitch[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorPitch
+                self.sensor_primary_altitude[p_idx] = xtf.packets[
+                    pyxtf.XTFHeaderType.sonar
+                ][p_idx].SensorPrimaryAltitude
+                self.sensor_roll[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorRoll
+                self.sensor_speed[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorSpeed
+                self.sensor_aux_altitude[p_idx] = xtf.packets[
+                    pyxtf.XTFHeaderType.sonar
+                ][p_idx].SensorAuxAltitude
+                self.longitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorXcoordinate
+                self.latitude[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorYcoordinate
+                self.depth[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].SensorDepth
+                self.packet_no[p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][
+                    p_idx
+                ].PingNumber
                 for ch_idx in range(self.num_ch):
-                    if len(xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].ping_chan_headers) == self.num_ch:
-                        self.seconds_per_ping[ch_idx, p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].ping_chan_headers[ch_idx].SecondsPerPing
-                        self.slant_range[ch_idx, p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].ping_chan_headers[ch_idx].SlantRange
+                    if (
+                        len(
+                            xtf.packets[pyxtf.XTFHeaderType.sonar][
+                                p_idx
+                            ].ping_chan_headers
+                        )
+                        == self.num_ch
+                    ):
+                        self.seconds_per_ping[ch_idx, p_idx] = (
+                            xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx]
+                            .ping_chan_headers[ch_idx]
+                            .SecondsPerPing
+                        )
+                        self.slant_range[ch_idx, p_idx] = (
+                            xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx]
+                            .ping_chan_headers[ch_idx]
+                            .SlantRange
+                        )
                     else:
-                        self.seconds_per_ping[ch_idx, p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].ping_chan_headers[0].SecondsPerPing
-                        self.slant_range[ch_idx, p_idx] = xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx].ping_chan_headers[0].SlantRange
+                        self.seconds_per_ping[ch_idx, p_idx] = (
+                            xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx]
+                            .ping_chan_headers[0]
+                            .SecondsPerPing
+                        )
+                        self.slant_range[ch_idx, p_idx] = (
+                            xtf.packets[pyxtf.XTFHeaderType.sonar][p_idx]
+                            .ping_chan_headers[0]
+                            .SlantRange
+                        )
 
             self.ping_x_axis = xtf.x_axis_m
-
 
         elif self.filepath.suffix.casefold() == ".jsf":
             jsf_file = JSFFile(self.filepath)
@@ -152,9 +197,11 @@ class SidescanFile:
                         self.subsys_names.append(packet.header.subsys_no)
 
             if len(self.subsys_names) != self.subsys_num:
-                print(f"Mismatch on subsys names: {self.subsys_names} and expected num: {self.subsys_num}")
+                print(
+                    f"Mismatch on subsys names: {self.subsys_names} and expected num: {self.subsys_num}"
+                )
                 self.subsys_num = len(self.subsys_names)
-            
+
             data_port = []
             data_star = []
             expected_idx = np.zeros(2, dtype=int)
@@ -168,16 +215,23 @@ class SidescanFile:
                             self.num_ch = packet.header.channel + 1
 
                         # check ping number
-                        if expected_idx[packet.header.channel] != packet.message.ping_no:
-                            print(f"Expected ping mismatch: Subsys: {packet.header.subsys_no}, Channel: {packet.header.channel}, Ping number: {packet.message.ping_no} - expected ping: {expected_idx[packet.header.channel]}")
-                            expected_idx[packet.header.channel] = packet.message.ping_no + 1
+                        if (
+                            expected_idx[packet.header.channel]
+                            != packet.message.ping_no
+                        ):
+                            print(
+                                f"Expected ping mismatch: Subsys: {packet.header.subsys_no}, Channel: {packet.header.channel}, Ping number: {packet.message.ping_no} - expected ping: {expected_idx[packet.header.channel]}"
+                            )
+                            expected_idx[packet.header.channel] = (
+                                packet.message.ping_no + 1
+                            )
                         else:
                             expected_idx[packet.header.channel] += 1
 
                         # portside
                         if packet.header.channel == 0:
                             data_port.append(packet.message.data)
-                        
+
                         # starboard
                         elif packet.header.channel == 1:
                             data_star.append(packet.message.data)
@@ -220,31 +274,47 @@ class SidescanFile:
             # TODO: check if all values are interpreted correctly
             for packet in jsf_file.packets:
                 if type(packet) == JSFSonarDataPacket:
-                    if packet.header.subsys_no == self.subsys_names[self.choose_subsys] and packet.header.channel == 0:
-                        
+                    if (
+                        packet.header.subsys_no == self.subsys_names[self.choose_subsys]
+                        and packet.header.channel == 0
+                    ):
+
                         self.sound_velocity[p_idx] = packet.message.SOS
-                        self.timestamp[p_idx] = datetime.fromtimestamp(packet.message.time)
-                        self.sensor_heading[p_idx] = packet.message.compass_heading / 100
+                        self.timestamp[p_idx] = datetime.fromtimestamp(
+                            packet.message.time
+                        )
+                        self.sensor_heading[p_idx] = (
+                            packet.message.compass_heading / 100
+                        )
                         self.sensor_pitch[p_idx] = packet.message.pitch / 32768 * 180
-                        self.sensor_primary_altitude[p_idx] = packet.message.altitude / 1e3
+                        self.sensor_primary_altitude[p_idx] = (
+                            packet.message.altitude / 1e3
+                        )
                         self.sensor_roll[p_idx] = packet.message.roll / 32768 * 180
                         self.sensor_speed[p_idx] = packet.message.speed / 10 / 1.944
-                        
+
                         self.depth[p_idx] = packet.message.depth / 1e3
                         self.packet_no[p_idx] = packet.message.packet_no
-                        self.seconds_per_ping[:, p_idx] = packet.message.sampling_interval_ns / 1e9
+                        self.seconds_per_ping[:, p_idx] = (
+                            packet.message.sampling_interval_ns / 1e9
+                        )
                         # check whether SOS is valid
                         sos = packet.message.SOS
                         if sos == 0:
                             sos = 1500
                         # round to 2 decimals here to avoid potential resampling of data in slant range correction
-                        self.slant_range[:, p_idx] = np.round(self.seconds_per_ping[0, p_idx] * self.ping_len * sos / 2, decimals=2)
+                        self.slant_range[:, p_idx] = np.round(
+                            self.seconds_per_ping[0, p_idx] * self.ping_len * sos / 2,
+                            decimals=2,
+                        )
                         self.starting_depth[p_idx] = packet.message.starting_depth
                         self.gain_adc[p_idx] = packet.message.gain_adc
 
                         if packet.message.coord_units == 2:
                             # convert Latitude, longitude in minutes of arc times 10000 to degree
-                            self.longitude[p_idx] = packet.message.longitude / 10000 / 60
+                            self.longitude[p_idx] = (
+                                packet.message.longitude / 10000 / 60
+                            )
                             self.latitude[p_idx] = packet.message.latitude / 10000 / 60
                         else:
                             # TODO: implement other cases:
@@ -253,7 +323,7 @@ class SidescanFile:
                             # 3 = X, Y in decimeters
                             # 4 = X, Y in centimeters
                             raise NotImplementedError
-                        
+
                         p_idx += 1
 
             self.ping_x_axis = np.linspace(0, self.slant_range[0][0], self.ping_len)

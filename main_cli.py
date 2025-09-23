012f834436b21f1b453b69407b6bd5176f94942a
finn <finn.spitz@sonoware.de>
2025-09-23 11:19:54 +0200

    Write pngs

--- main_cli.py
+++ main_cli.py
@@ -4,23 +4,24 @@ import yaml
 import numpy as np
 from sidescan_file import SidescanFile
 from sidescan_preproc import SidescanPreprocessor
-from sidescan_georeferencer import SidescanGeoreferencer
+from georef_thread import Georeferencer
 from custom_widgets import (
     convert_to_dB,
     hist_equalization,
-) # TODO: move these function somewhere else to remove Qt import here
+)  # TODO: move these function somewhere else to remove Qt import here
 from timeit import default_timer as timer
 
-class SidescanToolsMain():
+
+class SidescanToolsMain:
     filepath: Path
     cfg: dict
 
     def __init__(self, file_path, cfg_path):
         self.filepath = Path(file_path)
         f = open(
-                cfg_path,
-                "r",
-            )
+            cfg_path,
+            "r",
+        )
         loaded_dict = yaml.safe_load(f)
         f.close()
         self.cfg = loaded_dict
@@ -44,10 +45,14 @@ class SidescanToolsMain():
             downsampling_factor = bottom_info["downsampling_factor"]
         except:
             downsampling_factor = 1
-            
-        portside_bottom_dist = bottom_info["bottom_info_port"].flatten()[:sidescan_file.num_ping]
-        starboard_bottom_dist = bottom_info["bottom_info_star"].flatten()[:sidescan_file.num_ping]
-        
+
+        portside_bottom_dist = bottom_info["bottom_info_port"].flatten()[
+            : sidescan_file.num_ping
+        ]
+        starboard_bottom_dist = bottom_info["bottom_info_star"].flatten()[
+            : sidescan_file.num_ping
+        ]
+
         if downsampling_factor != 1:
             # rescale bottom info
             portside_bottom_dist = portside_bottom_dist * downsampling_factor
@@ -73,10 +78,14 @@ class SidescanToolsMain():
             (preproc.num_chunk, preproc.chunk_size), dtype=int
         )
         for chunk_idx in range(preproc.num_chunk):
-            port_chunk = portside_bottom_dist[chunk_idx * preproc.chunk_size:(chunk_idx+1) * preproc.chunk_size]
-            preproc.napari_portside_bottom[chunk_idx, :len(port_chunk)] = port_chunk
-            star_chunk = starboard_bottom_dist[chunk_idx * preproc.chunk_size:(chunk_idx+1) * preproc.chunk_size]
-            preproc.napari_starboard_bottom[chunk_idx, :len(star_chunk)] = star_chunk
+            port_chunk = portside_bottom_dist[
+                chunk_idx * preproc.chunk_size : (chunk_idx + 1) * preproc.chunk_size
+            ]
+            preproc.napari_portside_bottom[chunk_idx, : len(port_chunk)] = port_chunk
+            star_chunk = starboard_bottom_dist[
+                chunk_idx * preproc.chunk_size : (chunk_idx + 1) * preproc.chunk_size
+            ]
+            preproc.napari_starboard_bottom[chunk_idx, : len(star_chunk)] = star_chunk
 
         # --- Processing
         print(f"Pie slice filtering {self.filepath}")
@@ -99,16 +108,13 @@ class SidescanToolsMain():
                 preproc.sonar_data_proc[1],
             )
         )
-        end_timer_processing = timer()
 
-        # --- Georeferencing
-        start_timer_georef = timer()
-        ping_len = int(np.shape(preproc.egn_corrected_mat)[1]/2)
-        proc_data_0 = preproc.egn_corrected_mat[:, 0 : ping_len]
+        ping_len = int(np.shape(preproc.egn_corrected_mat)[1] / 2)
+        proc_data_0 = preproc.egn_corrected_mat[:, 0:ping_len]
         proc_data_0 = np.nan_to_num(
             proc_data_0
         )  # remove nans from excluding far/nadir unknown values
-        proc_data_1 = preproc.egn_corrected_mat[:, ping_len :]
+        proc_data_1 = preproc.egn_corrected_mat[:, ping_len:]
         proc_data_1 = np.nan_to_num(proc_data_1)
         # Convert data to dB
         proc_data_out_0 = convert_to_dB(proc_data_0)
@@ -116,8 +122,19 @@ class SidescanToolsMain():
         # Apply CLAHE
         proc_data_out_0 = hist_equalization(proc_data_out_0)
         proc_data_out_1 = hist_equalization(proc_data_out_1)
+
+        end_timer_processing = timer()
+
+        # Write png
+        Georeferencer.write_img(
+            self.cfg["Georef dir"] + self.filepath.stem + "_processed.png",
+            np.hstack((proc_data_out_0, proc_data_out_1)),
+        )
+
+        # --- Georeferencing
+        start_timer_georef = timer()
         # TODO: get settings from CFG
-        georeferencer = SidescanGeoreferencer(
+        georeferencer = Georeferencer(
             filepath=self.filepath,
             channel=0,
             dynamic_chunking=False,
@@ -127,7 +144,7 @@ class SidescanToolsMain():
             vertical_beam_angle=60,
         )
         georeferencer.process()
-        georeferencer = SidescanGeoreferencer(
+        georeferencer = Georeferencer(
             filepath=self.filepath,
             channel=1,
             dynamic_chunking=False,
@@ -142,7 +159,6 @@ class SidescanToolsMain():
         print(f"Georef took {end_timer_georef - start_timer_georef} s")
 
 
-        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tool to process sidescan sonar data")
     parser.add_argument("filepath", metavar="FILE", help="Path to xtf/jsf file")
@@ -154,4 +170,4 @@ if __name__ == "__main__":
     sidescantools = SidescanToolsMain(args.filepath, args.cfg)
     sidescantools.process()
 
-# T:\\projekte\\intern\\geisternetze\\seekuh2024_kiel\\990F\\StarfishLog_20240822_131816.xtf ./sidescan_out/project_info.yml
+# T:\\projekte\\intern\\geisternetze\\seekuh2024_kiel\\990F\\StarfishLog_20240822_131816.xtf U:\\git\\ghostnetdetector\\sidescan_out_kiel\\project_info.yml

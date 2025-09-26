dbec5c827a516b89561303047df997c795447766
finn <finn.spitz@sonoware.de>
2025-09-26 13:52:07 +0200

    Implement processing of all files in a folder

--- main_cli.py
+++ main_cli.py
@@ -17,145 +17,178 @@ ACTIVE_GEOREF = False
 
 class SidescanToolsMain:
     filepath: Path
+    cfg_path: Path
+    sidescan_files_path: list
     cfg: dict
 
     def __init__(self, file_path, cfg_path):
         self.filepath = Path(file_path)
-        f = open(
-            cfg_path,
-            "r",
-        )
-        loaded_dict = yaml.safe_load(f)
-        f.close()
-        self.cfg = loaded_dict
+        self.cfg_path = Path(cfg_path)
+        # Check input sidescan file path
+        if self.filepath.is_dir():
+            self.sidescan_files_path = []
+            for f_path in list(self.filepath.glob("*.xtf")):
+                self.sidescan_files_path.append(f_path)
+            for f_path in list(self.filepath.glob("*.xtf")):
+                self.sidescan_files_path.append(f_path)
+        elif self.filepath.is_file():
+            self.sidescan_files_path = [self.filepath]
+        else:
+            raise ValueError(
+                f"Argument path: {self.filepath} is not a valid directory or file."
+            )
 
-        # TODO: checkt that these files exist
+        # Read CFG
+        if self.cfg_path.is_file() and self.cfg_path.suffix == ".yml":
+            f = open(
+                cfg_path,
+                "r",
+            )
+            loaded_dict = yaml.safe_load(f)
+            f.close()
+            self.cfg = loaded_dict
+        else:
+            raise ValueError(
+                f"CFG can't be found or is no valid yml file: {self.cfg_path}"
+            )
+
+        # summarize found files which will be processed
+        print(
+            f"SidescanTools found the following sonar files:{self.sidescan_files_path}"
+        )
+        print(
+            f"Processed PNGs will be written to: {self.sidescan_files_path[0].parent}"
+        )
+        print(f"Georeferencing will be working in: {self.cfg["Georef dir"]}")
         # TODO: write cfg parser to make working with cfg parameters more convenient
 
     def process(self):
-        # TODO: get this from meta info and build functionalities to estimate bottom line without needing user input
-        # TODO: Structuring and so on...
 
-        # Load file
-        start_timer_processing = timer()
-        sidescan_file = SidescanFile(filepath=self.filepath)
+        # Process files
+        for sidescan_path in self.sidescan_files_path:
+            start_timer_processing = timer()
+            sidescan_file = SidescanFile(filepath=sidescan_path)
 
-        preproc = SidescanPreprocessor(
-            sidescan_file=sidescan_file,
-            chunk_size=self.cfg["Slant chunk size"],
-        )
-
-        # --- Processing
-        print(f"Pie slice filtering {self.filepath}")
-        preproc.apply_pie_slice_filter()
-        print(f"Slant range correcting {self.filepath}")
-        preproc.slant_range_correction(
-            nadir_angle=self.cfg["Slant nadir angle"],
-            use_intern_altitude=True,
-        )
-        # TODO: Decide whether to use EGN or BAC via CFG
-        # TODO: Therefore one could implement and incremental EGN Table building and corection
-        print(f"Apply BAC to {self.filepath}")
-        preproc.apply_beam_pattern_correction()
-        preproc.apply_energy_normalization()
-        preproc.egn_corrected_mat = np.hstack(
-            (
-                np.fliplr(preproc.sonar_data_proc[0]),
-                preproc.sonar_data_proc[1],
+            preproc = SidescanPreprocessor(
+                sidescan_file=sidescan_file,
+                chunk_size=self.cfg["Slant chunk size"],
             )
-        )
 
-        ping_len = int(np.shape(preproc.egn_corrected_mat)[1] / 2)
-        proc_data_0 = preproc.egn_corrected_mat[:, 0:ping_len]
-        proc_data_0 = np.nan_to_num(
-            proc_data_0
-        )  # remove nans from excluding far/nadir unknown values
-        proc_data_1 = preproc.egn_corrected_mat[:, ping_len:]
-        proc_data_1 = np.nan_to_num(proc_data_1)
-        # Convert data to dB
-        proc_data_out_0 = convert_to_dB(proc_data_0)
-        proc_data_out_1 = convert_to_dB(proc_data_1)
-        # Apply CLAHE
-        proc_data_out_0 = hist_equalization(proc_data_out_0)
-        proc_data_out_1 = hist_equalization(proc_data_out_1)
-
-        end_timer_processing = timer()
-
-        # --- Georeferencing
-        if ACTIVE_GEOREF:
-            start_timer_georef = timer()
-            # TODO: get settings from CFG
-            georeferencer = Georeferencer(
-                filepath=self.filepath,
-                channel=0,
-                output_folder=self.cfg["Georef dir"],
-                proc_data=proc_data_out_0,
-                vertical_beam_angle=60,
+            # --- Processing
+            print(f"Pie slice filtering {sidescan_path}")
+            preproc.apply_pie_slice_filter()
+            print(f"Slant range correcting {sidescan_path}")
+            preproc.slant_range_correction(
+                nadir_angle=self.cfg["Slant nadir angle"],
+                use_intern_altitude=True,
             )
-            georeferencer.process()
-            georeferencer = Georeferencer(
-                filepath=self.filepath,
-                channel=1,
-                output_folder=self.cfg["Georef dir"],
-                proc_data=proc_data_out_1,
-                vertical_beam_angle=60,
+            # TODO: "Nacharbeiten" der Flughöhe
+            # TODO: Decide whether to use EGN or BAC via CFG
+            print(f"Apply BAC to {sidescan_path}")
+            preproc.apply_beam_pattern_correction()
+            preproc.apply_energy_normalization()
+            preproc.egn_corrected_mat = np.hstack(
+                (
+                    np.fliplr(preproc.sonar_data_proc[0]),
+                    preproc.sonar_data_proc[1],
+                )
             )
-            georeferencer.process()
 
-            print(f"Cleaning ...")
-            for file in os.listdir(self.cfg["Georef dir"]):
-                file_path = os.path.join(self.cfg["Georef dir"], file)
-                if (
-                    str(file_path).endswith(".png")
-                    or str(file_path).endswith(".txt")
-                    or str(file_path).endswith("tmp.tif")
-                    or str(file_path).endswith(".points")
-                    or str(file_path).endswith(".xml")
-                    or str(file_path).endswith("tmp.csv")
-                ):
-                    try:
-                        os.remove(file_path)
-                    except FileNotFoundError:
-                        print(f"File Not Found: {file_path}")
+            ping_len = int(np.shape(preproc.egn_corrected_mat)[1] / 2)
+            proc_data_0 = preproc.egn_corrected_mat[:, 0:ping_len]
+            proc_data_0 = np.nan_to_num(
+                proc_data_0
+            )  # remove nans from excluding far/nadir unknown values
+            proc_data_1 = preproc.egn_corrected_mat[:, ping_len:]
+            proc_data_1 = np.nan_to_num(proc_data_1)
+            # Convert data to dB
+            proc_data_out_0 = convert_to_dB(proc_data_0)
+            proc_data_out_1 = convert_to_dB(proc_data_1)
+            # Apply CLAHE
+            proc_data_out_0 = hist_equalization(proc_data_out_0)
+            proc_data_out_1 = hist_equalization(proc_data_out_1)
 
-            print("Cleanup done")
+            end_timer_processing = timer()
 
-            end_timer_georef = timer()
+            # --- Georeferencing
+            if ACTIVE_GEOREF:
+                start_timer_georef = timer()
+                # TODO: get settings from CFG
+                georeferencer = Georeferencer(
+                    filepath=sidescan_path,
+                    channel=0,
+                    output_folder=self.cfg["Georef dir"],
+                    proc_data=proc_data_out_0,
+                    vertical_beam_angle=60,
+                )
+                georeferencer.process()
+                georeferencer = Georeferencer(
+                    filepath=sidescan_path,
+                    channel=1,
+                    output_folder=self.cfg["Georef dir"],
+                    proc_data=proc_data_out_1,
+                    vertical_beam_angle=60,
+                )
+                georeferencer.process()
 
-        # Also write processed data as png for comparison
-        img_data = np.hstack((proc_data_out_0, proc_data_out_1))
-        img_data /= np.max(np.abs(img_data))
-        img_data *= 255
-        Georeferencer.write_img(
-            self.filepath.parent / (self.filepath.stem + "_processed.png"),
-            img_data.astype(np.uint8),
-        )
+                print(f"Cleaning ...")
+                for file in os.listdir(self.cfg["Georef dir"]):
+                    file_path = os.path.join(self.cfg["Georef dir"], file)
+                    if (
+                        str(file_path).endswith(".png")
+                        or str(file_path).endswith(".txt")
+                        or str(file_path).endswith("tmp.tif")
+                        or str(file_path).endswith(".points")
+                        or str(file_path).endswith(".xml")
+                        or str(file_path).endswith("tmp.csv")
+                    ):
+                        try:
+                            os.remove(file_path)
+                        except FileNotFoundError:
+                            print(f"File Not Found: {file_path}")
 
-        # Plotting to find errors and test bottom line strategy
-        btm_line_mat_port = np.zeros_like(sidescan_file.data[0])
-        btm_line_mat_star = np.zeros_like(sidescan_file.data[0])
-        for line_idx in range(len(preproc.portside_bottom_dist.astype(int))):
-            btm_line_mat_port[
-                line_idx, preproc.portside_bottom_dist.astype(int)[line_idx]
-            ] = 1
-            btm_line_mat_star[
-                line_idx, preproc.starboard_bottom_dist.astype(int)[line_idx]
-            ] = 1
-        overlay_cmap = ListedColormap(["none", "red"])
-        plt.figure()
-        plt.imshow(20 * np.log10(sidescan_file.data[0]))
-        plt.imshow(btm_line_mat_port, cmap=overlay_cmap)
+                print("Cleanup done")
 
-        plt.figure()
-        plt.imshow(20 * np.log10(sidescan_file.data[1]))
-        plt.imshow(btm_line_mat_star, cmap=overlay_cmap)
+                end_timer_georef = timer()
 
-        plt.figure()
-        plt.imshow(img_data)
+            # Also write processed data as png for comparison
+            proc_data_out_0 /= np.max(np.abs(proc_data_out_0))
+            proc_data_out_0 *= 255
+            proc_data_out_1 /= np.max(np.abs(proc_data_out_1))
+            proc_data_out_1 *= 255
+            img_data = np.hstack((proc_data_out_0, proc_data_out_1))
+            Georeferencer.write_img(
+                sidescan_path.parent / (sidescan_path.stem + "_processed.png"),
+                img_data.astype(np.uint8),
+            )
+
+            # Plotting to find errors and test bottom line strategy
+            btm_line_mat_port = np.zeros_like(sidescan_file.data[0])
+            btm_line_mat_star = np.zeros_like(sidescan_file.data[0])
+            for line_idx in range(len(preproc.portside_bottom_dist.astype(int))):
+                btm_line_mat_port[
+                    line_idx, preproc.portside_bottom_dist.astype(int)[line_idx]
+                ] = 1
+                btm_line_mat_star[
+                    line_idx, preproc.starboard_bottom_dist.astype(int)[line_idx]
+                ] = 1
+            overlay_cmap = ListedColormap(["none", "red"])
+            plt.figure()
+            plt.imshow(20 * np.log10(sidescan_file.data[0]))
+            plt.imshow(btm_line_mat_port, cmap=overlay_cmap)
+
+            plt.figure()
+            plt.imshow(20 * np.log10(sidescan_file.data[1]))
+            plt.imshow(btm_line_mat_star, cmap=overlay_cmap)
+
+            plt.figure()
+            plt.imshow(img_data)
+
+            print(f"Processing took {end_timer_processing - start_timer_processing} s")
+            if ACTIVE_GEOREF:
+                print(f"Georef took {end_timer_georef - start_timer_georef} s")
+
         plt.show(block=True)
-
-        print(f"Processing took {end_timer_processing - start_timer_processing} s")
-        print(f"Georef took {end_timer_georef - start_timer_georef} s")
 
 
 if __name__ == "__main__":
